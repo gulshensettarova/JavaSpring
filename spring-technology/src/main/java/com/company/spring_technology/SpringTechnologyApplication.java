dadcadd24620c1ef583d539500368cb18a7bcb19
gulshensettarova <gulshensettarova@gmail.com>
2024-10-03 11:51:31 +0400

    Initial Project

---
+++ spring-technology/src/main/java/com/company/spring_technology/SpringTechnologyApplication.java
@@ -0,0 +1,13 @@
+package com.company.spring_technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTechnologyApplication.class, args);
+	}
+
+}
---
+++ spring-technology/src/test/java/com/company/spring_technology/SpringTechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.spring_technology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTechnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
